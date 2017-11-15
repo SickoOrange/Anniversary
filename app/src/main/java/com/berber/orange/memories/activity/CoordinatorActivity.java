89faf23522f68656662b550850eea599bcdd6e21
SickoOrange <181536584@qq.com>
2017-11-15 21:25:25 +0100

    toobar add action menu

--- app/src/main/java/com/berber/orange/memories/activity/CoordinatorActivity.java
+++ app/src/main/java/com/berber/orange/memories/activity/CoordinatorActivity.java
@@ -187,7 +187,7 @@ public class CoordinatorActivity extends BaseActivity implements NavigationView.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.test, menu);
+        getMenuInflater().inflate(R.menu.menu_coordinator, menu);
         return true;
     }
 
@@ -196,12 +196,19 @@ public class CoordinatorActivity extends BaseActivity implements NavigationView.
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_add:
+                Utils.showToast(CoordinatorActivity.this, "add new item", 0);
+                return true;
         }
+
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
 
         return super.onOptionsItemSelected(item);
     }
