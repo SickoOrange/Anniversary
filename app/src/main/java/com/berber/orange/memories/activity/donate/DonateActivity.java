3312073874550978a5bbfbcb0ec55e2d242df242
SickoOrange <181536584@qq.com>
2017-12-03 16:53:59 +0100

    优化

--- app/src/main/java/com/berber/orange/memories/activity/donate/DonateActivity.java
+++ app/src/main/java/com/berber/orange/memories/activity/donate/DonateActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.berber.orange.memories.R;
-import com.berber.orange.memories.billing.IabBroadcastReceiver;
 
 public class DonateActivity extends AppCompatActivity{
 
